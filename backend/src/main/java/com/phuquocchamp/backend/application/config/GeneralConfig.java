aa3972d6a7e5d877f723b4da606c15d322b23da7
HOÀNG TẤN PHÚ QUỐC <hoangtanphuquoc@gmail.com>
2025-04-14 15:10:51 +0700

    import modle mapper

---
+++ backend/src/main/java/com/phuquocchamp/backend/application/config/GeneralConfig.java
@@ -0,0 +1,13 @@
+package com.phuquocchamp.backend.application.config;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GeneralConfig {
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
+}
